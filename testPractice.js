5c2bf1d75d8c0f370823deaa6050b4b8c831da04
Carson <c_halter@live.ca>
2022-11-27 21:00:34 -0600

    rest of tests

--- testPractice.js
+++ testPractice.js
@@ -2,4 +2,144 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-module.exports = capitalize;
+function reverseString(str) {
+  let newString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+
+const calculator = {
+  add(a, b) {
+    let sum = a + b;
+    return sum;
+  },
+
+  subtract(a, b) {
+    let sum = a - b;
+    return sum;
+  },
+
+  divide(a, b) {
+    let answer = a / b;
+    return answer;
+  },
+
+  multiply(a, b) {
+    let product = a * b;
+    return product;
+  },
+};
+
+function caesarCipher(string) {
+  let alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  // scipher is every letter shifted by 1
+  let currentLetter;
+  let encryptedLetter;
+  let encryption = '';
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (string[i] === 'z') {
+      encryptedLetter = alphabet[0];
+    } else if (string[i] === 'Z') {
+      encryptedLetter = alphabet[26];
+    } else {
+      let j = 0;
+      currentLetter = string[i];
+      while (currentLetter != alphabet[j]) {
+        j++;
+      }
+      encryptedLetter = alphabet[j + 1];
+    }
+
+    encryption += encryptedLetter;
+  }
+  return encryption;
+}
+
+function analyzeArray(array) {
+  const sum = array.reduce((prev, cur) => cur + prev, 0);
+
+  const average = sum / array.length;
+
+  const min = array.reduce((current, previous) => {
+    return Math.min(current, previous);
+  });
+
+  const max = array.reduce((current, previous) => {
+    return Math.max(current, previous);
+  });
+
+  const length = array.length;
+
+  const analyzedArray = {
+    average: average,
+    min: min,
+    max: max,
+    length: length,
+  };
+
+  return analyzedArray;
+}
+
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- testPractice.test.js
+++ testPractice.test.js
@@ -1,5 +1,42 @@
-const capitalize = require('./testPractice');
+const testModule = require('./testPractice');
 
 test('Capitalizes first letter of string', () => {
-  expect(capitalize('banana')).toMatch('Banana');
+  expect(testModule.capitalize('banana')).toMatch('Banana');
+});
+
+test('Takes a string and returns a reversed version', () => {
+  expect(testModule.reverseString('house')).toMatch('esuoh');
+});
+
+test('Adds two numbers', () => {
+  expect(testModule.calculator.add(1, 2)).toBe(3);
+});
+
+test('Subtracts two numbers', () => {
+  expect(testModule.calculator.subtract(7, 5)).toBe(2);
+});
+
+test('Divides two numbers', () => {
+  expect(testModule.calculator.divide(15, 5)).toBe(3);
+});
+
+test('Multiplies two numbers', () => {
+  expect(testModule.calculator.multiply(4, 5)).toBe(20);
+});
+
+test('ceasarCipher: test wrapping from z to a', () => {
+  expect(testModule.caesarCipher('zoo')).toMatch('app');
+});
+
+test('caesarCipher: test keeping same case', () => {
+  expect(testModule.caesarCipher('CarSon')).toMatch('DbsTpo');
+});
+
+test('analyzeArray: tests array parameters', () => {
+  expect(testModule.analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
